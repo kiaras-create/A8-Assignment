8f59cf48fad8c84d692c06784dbff401d313ff58
kiaras-create <ksunilsingh@smith.edu>
2025-11-14 13:09:50 -0500

    write method

--- A8-GuessingGame/BinaryTree.java
+++ A8-GuessingGame/BinaryTree.java
@@ -1,3 +1,7 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  *  Implements binary trees.
  *
@@ -120,5 +124,42 @@ public class BinaryTree<E> {
         } else {
             return "("+postorderString(t.left)+" "+postorderString(t.right)+" "+t.data+")";
         }
+    }
+
+    public void writeTree(String fileName){
+        BinaryTree<E> current = this;
+        // Create a node queue
+        java.util.LinkedList<BinaryTree<E>> nodeQueue = new java.util.LinkedList<>();
+        java.util.LinkedList<BinaryTree<E>> pathQueue = new java.util.LinkedList<>();
+        // boolean[] visited = new boolean[V];
+        //Add first node to linked list 
+        nodeQueue.addLast(this);
+        while (!nodeQueue.isEmpty()){
+            BinaryTree<E> currentNode = nodeQueue.removeFirst();
+            pathQueue.addLast(currentNode);
+            if(currentNode.getLeft() != null){
+                nodeQueue.addLast(currentNode.getLeft());
+            } 
+            if(currentNode.getRight() != null){
+                nodeQueue.addLast(currentNode.getRight());
+            }
+        }
+        try {
+        PrintWriter out = new PrintWriter(new FileWriter(fileName));
+
+        while(!pathQueue.isEmpty()){
+            out.println(pathQueue.removeFirst().toString());
+        }
+        out.close();
+        }
+        catch (IOException e) {
+            System.out.println("Problem writing the file: " + e.getMessage());
+
+
+        }
+    }
+    
+    public void readTree(String fileName){
+
     }
 }
