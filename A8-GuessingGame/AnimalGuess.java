8f2eacf6304c435e2172507407f4d3f60750d334
kiaras-create <ksunilsingh@smith.edu>
2025-12-18 19:33:29 -0500

    Fixed game loop and added question

--- A8-GuessingGame/AnimalGuess.java
+++ A8-GuessingGame/AnimalGuess.java
@@ -1,137 +1,200 @@
 import java.util.Scanner;
 
+
 public class AnimalGuess {
 
-    //Reads user input
-    public static Scanner scanner = new Scanner(System.in);
 
-    /**
-     * handles responses to nonsensical inputs
-     * 
-     * @param input
-     * @return nothing
-     */
-    // private static void handlingResponses(String input) {
-    //     // converts input to all lowercase
-    //     input.toLowerCase();
-    //     // if input doesn't equal yes or no, then ask player to reenter answer
-    //     if ((!input.equals("yes")) || (!input.equals("no"))) {
-    //         System.out.println("Please reenter your answer.");
-    //         // TO-DO: potentially redirect this code to the scanner in whichever method it
-    //         // is from/relevant
-    //         // call shakila's method that elicits a yes/no answer from user w/same question
-    //         // being asked
-    //     }
-    // }
+   //Reads user input
+   public static Scanner scanner = new Scanner(System.in);
+
+
+   /**
+    * handles responses to nonsensical inputs
+    *
+    * @param input
+    * @return nothing
+    */
+   // private static void handlingResponses(String input) {
+   //     // converts input to all lowercase
+   //     input.toLowerCase();
+   //     // if input doesn't equal yes or no, then ask player to reenter answer
+   //     if ((!input.equals("yes")) || (!input.equals("no"))) {
+   //         System.out.println("Please reenter your answer.");
+   //         // TO-DO: potentially redirect this code to the scanner in whichever method it
+   //         // is from/relevant
+   //         // call shakila's method that elicits a yes/no answer from user w/same question
+   //         // being asked
+   //     }
+   // }
+
 
 /**
- * Asks the user a yes/no question and handles invalid input by asking for input again and again until entered a valid input
- * it accepts 'yes', 'y', 'no', or 'n'
- * Responds gracefully to invalid inputs
- * 
- * @param question the yes/no question to ask the user
- * @return true if the user answers 'yes' or 'y' and false if 'no' or 'n'
- */
+* Asks the user a yes/no question and handles invalid input by asking for input again and again until entered a valid input
+* it accepts 'yes', 'y', 'no', or 'n'
+* Responds gracefully to invalid inputs
+*
+* @param question the yes/no question to ask the user
+* @return true if the user answers 'yes' or 'y' and false if 'no' or 'n'
+*/
 public static boolean getYesOrNo(String question){
 
-    //Loop for until valid input (yes/y, no/n) received
-    while(true) {
 
-         //Prints the question for the user plus two answer options (yes/no)
-        System.out.println(question + "(yes/no):");
+   //Loop for until valid input (yes/y, no/n) received
+   while(true) {
 
-        String input = scanner.nextLine();
 
-        //convert the input to lowercase letter
-        input = input.toLowerCase();
+        //Prints the question for the user plus two answer options (yes/no)
+       System.out.println(question + "(yes/no):");
 
-        //Check if the input is yes
-        if (input.equals("yes") || input.equals("y")){
-            return true;
 
-        //Check if the input is no
-        } else if (input.equals("no") || input.equals("n")){
-            return false;
-        } else {
+       String input = scanner.nextLine();
 
-            //Prints a message if invalid answer entered
-            System.out.println("Please answer with yes or no"); 
-        }
-    }}
 
-    /**
-     * Read a line of input with handling the error
-     * 
-     * @param question the question to display to the user
-     * @return user input
-     */
-    public static String readLine(String question){
-        System.out.print(question); //Prints the question for the user
-        String input = ""; //empty string placeholder
-        while(true){
-            try{
-                input = scanner.nextLine();
+       //convert the input to lowercase letter
+       input = input.toLowerCase();
 
-                //Checks for empty input
-                if (input.isEmpty()){
-                    throw new IllegalArgumentException("Input cannot be empty");
-                }
-                break;
-            } catch (IllegalArgumentException e){
-                System.out.println("Please try again");
-                System.out.println(question); //Prints the question again
-            }
-        }
-        return input;
-    }
 
-    /**
-     * extends decision tree when program fails to guess correct animal
-     * method occurs after program guesses and answer is no
-     * 
-     * @param node, the leaf node that contains the incorrect answer
-     * @return nothing
-     */
-    private static void extendDecisionTree(String wrongAnimal, BinaryTree<String> node) {
-        // asks user what animal they were thinking of
-        System.out.println("Oh no, I couldn't guess your animal! It's not a " + wrongAnimal + ".");
-        System.out.println("What animal were you thinking of?");
-        String correctAnimal = scanner.nextLine();
+       //Check if the input is yes
+       if (input.equals("yes") || input.equals("y")){
+           return true;
 
-        // asks user distinguishing question and sets the data of node to question
-        String question = "Is the animal you were thinking of a mammal?";
-        System.out.println(question);
-        node.setData(question);
-        // user answers and convert answer to all lower case
-        String answer = scanner.nextLine();
-        answer.toLowerCase();
 
-        // if answer is yes, create left child node with user's animal
-        if (answer.equals("yes")) {
-            BinaryTree<String> lChild = new BinaryTree<>(correctAnimal);
-            node.setLeft(lChild);
-            // BinaryTree<String> rChild = new BinaryTree<>(wrongAnimal);
-            // node.setRight(rChild);
-            // if answer is no, create right child w/user's animal
-        } else if (answer.equals("no")) {
-            BinaryTree<String> rChild = new BinaryTree<>(correctAnimal);
-            node.setLeft(rChild);
-            // BinaryTree<String> lChild = new BinaryTree<>(wrongAnimal);
-            // otherwise, call handlingResponses method to handle nonsensical inputs
-        } else {
-            readLine(question);
-        }
+       //Check if the input is no
+       } else if (input.equals("no") || input.equals("n")){
+           return false;
+       } else {
 
-    }
 
-    //For testing purposes
-    public static void main(String[] args){
-        boolean answer = getYesOrNo("Is your animal a Crocodile?");
-        if (answer){
-            System.out.println("Wow that's cool.");
-        } else{
-            System.out.println("Hm, I see.");
-        }
-    }
+           //Prints a message if invalid answer entered
+           System.out.println("Please answer with yes or no");
+       }
+   }}
+
+
+   /**
+    * Read a line of input with handling the error
+    *
+    * @param question the question to display to the user
+    * @return user input
+    */
+   public static String readLine(String question){
+       System.out.print(question); //Prints the question for the user
+       String input = ""; //empty string placeholder
+       while(true){
+           try{
+               input = scanner.nextLine();
+
+
+               //Checks for empty input
+               if (input.isEmpty()){
+                   throw new IllegalArgumentException("Input cannot be empty");
+               }
+               break;
+           } catch (IllegalArgumentException e){
+               System.out.println("Please try again");
+               System.out.println(question); //Prints the question again
+           }
+       }
+       return input;
+   }
+
+
+   /**
+    * extends decision tree when program fails to guess correct animal
+    * method occurs after program guesses and answer is no
+    *
+    * @param node, the leaf node that contains the incorrect answer
+    * @return nothing
+    */
+   private static void extendDecisionTree(String wrongAnimal, DecisionTree node) {
+       // asks user what animal they were thinking of
+       System.out.println("Oh no, I couldn't guess your animal! It's not a " + wrongAnimal + ".");
+       System.out.println("What animal were you thinking of?");
+       String correctAnimal = scanner.nextLine();
+
+
+       // asks user distinguishing question and sets the data of node to question
+       String questionPrompt = "Type a yes or no question that distinguishes between the" + " " + correctAnimal + " " + "and the " + wrongAnimal + ":";
+       System.out.println(questionPrompt);
+       //node.setData(question);
+       // user answers and convert answer to all lower case
+       String question = scanner.nextLine();
+       node.setData(question);
+
+
+       boolean userYes = getYesOrNo("Is the answer to this question yes for this animal: " + correctAnimal );
+       if (userYes){
+           node.setLeft(new DecisionTree(correctAnimal));
+           node.setRight(new DecisionTree(wrongAnimal));
+       } else{
+           node.setLeft(new DecisionTree(wrongAnimal));
+           node.setRight(new DecisionTree(correctAnimal));
+       }
+       //     answer.toLowerCase();
+
+
+   //     // if answer is yes, create left child node with user's animal
+   //     if (answer.equals("yes")) {
+   //         BinaryTree<String> lChild = new BinaryTree<>(correctAnimal);
+   //         node.setLeft(lChild);
+   //         // BinaryTree<String> rChild = new BinaryTree<>(wrongAnimal);
+   //         // node.setRight(rChild);
+   //         // if answer is no, create right child w/user's animal
+   //     } else if (answer.equals("no")) {
+   //         BinaryTree<String> rChild = new BinaryTree<>(correctAnimal);
+   //         node.setLeft(rChild);
+   //         // BinaryTree<String> lChild = new BinaryTree<>(wrongAnimal);
+   //         // otherwise, call handlingResponses method to handle nonsensical inputs
+   //     } else {
+   //         readLine(question);
+   //     }
+
+
+   }
+
+
+   //For testing purposes
+   public static void main(String[] args){
+       String fileName = args[0];
+
+
+       DecisionTree root = new DecisionTree((String) null);
+       DecisionTree.readTree(fileName, root);
+
+
+       System.out.println("Think of an animal and the computer will attempt to guess it");
+
+
+       DecisionTree current = root;
+
+
+       while (current.getLeft() != null || current.getRight() != null ){
+           boolean yes = getYesOrNo(current.getData());
+           if (yes){
+               current = current.getLeft();
+           } else {
+               current = current.getRight();
+           }
+       }
+       boolean guess = getYesOrNo("Is the animal" + current.getData() + "?");
+       if (guess){
+           System.out.println("Computer guess is correct!");
+       } else {
+           extendDecisionTree(current.getData(), current);
+       }
+       System.out.println("Done");
+   //     boolean answer = getYesOrNo("Is your animal a Crocodile?");
+   //     if (answer){
+   //         System.out.println("Wow that's cool.");
+   //     } else{
+   //         System.out.println("Hm, I see.");
+   //     }
+   }
+
+
+
+
+
 
 }
+
--- A8-GuessingGame/DecisionTree.java
+++ A8-GuessingGame/DecisionTree.java
@@ -3,200 +3,239 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
+
 public class DecisionTree extends BinaryTree<String> {
 
-    /**
-     * @param data
-     */
-    public DecisionTree(String data) {
-        super(data);
-    }
 
-    /**
-     * @param data
-     * @param left
-     * @param right
-     */
-    public DecisionTree(String data, BinaryTree<String> left, BinaryTree<String> right) {
-        super(data, left, right);
-    }
+   /**
+    * @param data
+    */
+   public DecisionTree(String data) {
+       super(data);
+   }
 
-    /**
-     * @param other
-     */
-    public DecisionTree(DecisionTree other) {
-        super(other);
-    }
 
-    // overwriting specialized methods
-    @Override
-    public DecisionTree getLeft() {
-        return (DecisionTree) super.getLeft();
-    }
+   /**
+    * @param data
+    * @param left
+    * @param right
+    */
+   public DecisionTree(String data, BinaryTree<String> left, BinaryTree<String> right) {
+       super(data, left, right);
+   }
 
-    @Override
-    public DecisionTree getRight() {
-        return (DecisionTree) super.getRight();
-    }
 
-    @Override
-    public void setLeft(BinaryTree<String> left) {
-        super.setLeft(left);
-    }
+   /**
+    * @param other
+    */
+   public DecisionTree(DecisionTree other) {
+       super(other);
+   }
 
-    /**
-     * @param left
-     */
-    public void setLeft(DecisionTree left) {
-        super.setLeft(left);
-    }
 
-    @Override
-    public void setRight(BinaryTree<String> right) {
-        super.setRight(right);
-    }
+   // overwriting specialized methods
+   @Override
+   public DecisionTree getLeft() {
+       return (DecisionTree) super.getLeft();
+   }
 
-    /**
-     * @param right
-     */
-    public void setRight(DecisionTree right) {
-        super.setRight(right);
-    }
 
-    /**
-     *
-     * @param path
-     *             Moves through the tree and follows path
-     * @return current
-     */
-    public BinaryTree<String> followPath(String path) {
-        BinaryTree<String> current = this; // Start at root
-        for (int i = 0; i < path.length(); i++) {
+   @Override
+   public DecisionTree getRight() {
+       return (DecisionTree) super.getRight();
+   }
 
-            if (path.charAt(i) == 'Y') { // If answer is yes move left
-                current = current.getLeft(); // Update current
-            } else if (path.charAt(i) == 'N') { // If answer is no move right
-                current = current.getRight(); // Update current
 
-            }
+   @Override
+   public void setLeft(BinaryTree<String> left) {
+       super.setLeft(left);
+   }
 
-        }
-        return current;
-    }
 
-    public static void writeTree(String fileName, BinaryTree<String> inputNode) {
-        // BinaryTree<String> current = this;
-        // Create a node queue
-        java.util.LinkedList<BinaryTree<String>> nodeQueue = new java.util.LinkedList<>();
-        java.util.LinkedList<String> pathQueue = new java.util.LinkedList<>();
-        java.util.ArrayList<String> finalLines = new java.util.ArrayList<>();
-        // boolean[] visited = new boolean[V];
-        // Add first node to linked list
-        nodeQueue.addLast(inputNode);
-        pathQueue.addLast("");
-        while (!nodeQueue.isEmpty()) {
-            BinaryTree<String> currentNode = nodeQueue.removeFirst();
-            String currentPath = pathQueue.removeFirst();
+   /**
+    * @param left
+    */
+   public void setLeft(DecisionTree left) {
+       super.setLeft(left);
+   }
 
-            if (currentNode.getLeft() != null) {
-                nodeQueue.addLast(currentNode.getLeft());
-                String leftPath = currentPath + "Y";
-                pathQueue.add(leftPath);
-            }
-            if (currentNode.getRight() != null) {
-                nodeQueue.addLast(currentNode.getRight());
-                String rightPath = currentPath + "N";
-                pathQueue.add(rightPath);
-            }
-            String finalPath = currentPath + " " + currentNode.getData();
-            finalLines.add(finalPath);
-        }
 
-        try {
-            PrintWriter out = new PrintWriter(new FileWriter(fileName));
+   @Override
+   public void setRight(BinaryTree<String> right) {
+       super.setRight(right);
+   }
 
-            for (int i = 0; i < finalLines.size(); i++) {
-                out.println(finalLines.get(i));
-            }
 
-            out.close();
-        } catch (IOException e) {
-            System.out.println("Problem writing the file: " + e.getMessage());
+   /**
+    * @param right
+    */
+   public void setRight(DecisionTree right) {
+       super.setRight(right);
+   }
 
-        }
 
-    }
+   /**
+    *
+    * @param path
+    *             Moves through the tree and follows path
+    * @return current
+    */
+   public DecisionTree followPath(String path) {
+       DecisionTree current = this; // Start at root
+       for (int i = 0; i < path.length(); i++) {
 
-    public static void readTree(String fileName, DecisionTree root) {
-        try {
-            Scanner file = new Scanner(new java.io.File(fileName));
-            while (file.hasNextLine()) {
-                String line = file.nextLine();
-                int spaceIndex = line.indexOf(" ");
-                int length = line.length();
-                String path = line.substring(0, spaceIndex);
-                String data = line.substring(spaceIndex + 1, length);
-                DecisionTree current = root;
 
-                if (path.isEmpty()) {
-                    root.setData(data);
-                    continue;
-                }
+           if (path.charAt(i) == 'Y') { // If answer is yes move left
+               current = current.getLeft(); // Update current
+           } else if (path.charAt(i) == 'N') { // If answer is no move right
+               current = current.getRight(); // Update current
 
-                String parentPath = path.substring(0, path.length() - 1);
-                char lastCharacter = path.charAt(path.length() - 1);
 
-                for (int i = 0; i < parentPath.length(); i++) {
-                    char character = parentPath.charAt(i);
-                    if (character == 'Y') {
-                        if (current.getLeft() == null) {
-                            current.setLeft(new DecisionTree((String) null));
+           }
 
-                        }
-                        current = current.getLeft();
-                    }
-                    if (character == 'N') {
-                        if (current.getRight() == null) {
-                            current.setRight(new DecisionTree((String) null));
-                        }
-                        current = current.getRight();
 
-                    }
-                    DecisionTree newNode = new DecisionTree(data);
+       }
+       return current;
+   }
 
-                    if (lastCharacter == 'Y') {
-                        current.setLeft(newNode);
-                    } else if (lastCharacter == 'N') {
-                        current.setRight(newNode);
-                    }
 
-                }
-            }
+   public static void writeTree(String fileName,DecisionTree inputNode) {
+       // BinaryTree<String> current = this;
+       // Create a node queue
+       java.util.LinkedList<BinaryTree<String>> nodeQueue = new java.util.LinkedList<>();
+       java.util.LinkedList<String> pathQueue = new java.util.LinkedList<>();
+       java.util.ArrayList<String> finalLines = new java.util.ArrayList<>();
+       // boolean[] visited = new boolean[V];
+       // Add first node to linked list
+       nodeQueue.addLast(inputNode);
+       pathQueue.addLast("");
+       while (!nodeQueue.isEmpty()) {
+           BinaryTree<String> currentNode = nodeQueue.removeFirst();
+           String currentPath = pathQueue.removeFirst();
 
-            file.close();
-        } catch (Exception e) {
-            System.out.println("cannot make file" + e);
 
-        }
+           if (currentNode.getLeft() != null) {
+               nodeQueue.addLast(currentNode.getLeft());
+               String leftPath = currentPath + "Y";
+               pathQueue.add(leftPath);
+           }
+           if (currentNode.getRight() != null) {
+               nodeQueue.addLast(currentNode.getRight());
+               String rightPath = currentPath + "N";
+               pathQueue.add(rightPath);
+           }
+           String finalPath = currentPath + " " + currentNode.getData();
+           finalLines.add(finalPath);
+       }
 
-    }
 
-    // Sample decision tree with 7 nodes
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        DecisionTree cow = new DecisionTree("Cow");
-        DecisionTree pig = new DecisionTree("Pig");
-        DecisionTree snake = new DecisionTree("Snake");
-        DecisionTree lizard = new DecisionTree("Lizard");
+       try {
+           PrintWriter out = new PrintWriter(new FileWriter(fileName));
 
-        DecisionTree hooves = new DecisionTree("Does it have hooves?", cow, lizard);
-        DecisionTree reptile = new DecisionTree("Is it a reptile?", snake, pig);
 
-        DecisionTree root = new DecisionTree("Is it a mammal?", hooves, reptile);
+           for (int i = 0; i < finalLines.size(); i++) {
+               out.println(finalLines.get(i));
+           }
 
-        writeTree("Animal.txt", root);
-        readTree("Animal.txt", root);
-    }
+
+           out.close();
+       } catch (IOException e) {
+           System.out.println("Problem writing the file: " + e.getMessage());
+
+
+       }
+
+
+   }
+
+
+   public static void readTree(String fileName, DecisionTree root) {
+       try {
+           Scanner file = new Scanner(new java.io.File(fileName));
+           while (file.hasNextLine()) {
+               String line = file.nextLine();
+               int spaceIndex = line.indexOf(" ");
+               int length = line.length();
+               String path = line.substring(0, spaceIndex);
+               String data = line.substring(spaceIndex + 1, length);
+               DecisionTree current = root;
+
+
+               if (path.isEmpty()) {
+                   root.setData(data);
+                   continue;
+               }
+
+
+               String parentPath = path.substring(0, path.length() - 1);
+               char lastCharacter = path.charAt(path.length() - 1);
+
+
+               for (int i = 0; i < parentPath.length(); i++) {
+                   char character = parentPath.charAt(i);
+                   if (character == 'Y') {
+                       if (current.getLeft() == null) {
+                           current.setLeft(new DecisionTree((String) null));
+
+
+                       }
+                       current = current.getLeft();
+                   }
+                   else if (character == 'N') {
+                       if (current.getRight() == null) {
+                           current.setRight(new DecisionTree((String) null));
+                       }
+                       current = current.getRight();
+
+
+                   }
+
+
+               }
+               DecisionTree newNode = new DecisionTree(data);
+
+
+               if (lastCharacter == 'Y') {
+                   current.setLeft(newNode);
+               } else if (lastCharacter == 'N') {
+                       current.setRight(newNode);
+               }
+           }
+
+
+           file.close();
+       } catch (Exception e) {
+           System.out.println("cannot make file" + e);
+
+
+       }
+
+
+   }
+
+
+   // Sample decision tree with 7 nodes
+   /**
+    * @param args
+    */
+   public static void main(String[] args) {
+       DecisionTree cow = new DecisionTree("Cow");
+       DecisionTree pig = new DecisionTree("Pig");
+       DecisionTree snake = new DecisionTree("Snake");
+       DecisionTree lizard = new DecisionTree("Lizard");
+
+
+       DecisionTree hooves = new DecisionTree("Does it have hooves?", cow, lizard);
+       DecisionTree reptile = new DecisionTree("Is it a reptile?", snake, pig);
+
+
+       DecisionTree root = new DecisionTree("Is it a mammal?", hooves, reptile);
+
+
+       // writeTree("Animal.txt", root);
+       // readTree("Animal.txt", root);
+   }
 }
+
+
+
